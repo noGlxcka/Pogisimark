3efd83f21938a476b5c0a2fe6cf0b8344f0b22b5
GlxckADev <159496597+noGlxcka@users.noreply.github.com>
2024-07-27 11:56:07 +0800

    Create auth_config.lua

---
+++ auth_config.lua
@@ -0,0 +1,10 @@
+return {
+    ["GAR-1FMPT-VFHT"] = {
+        user_id = 1037396,
+        username = "AYOspaceGlo"
+    },
+    ["GAR-69-69"] = {
+        user_id = 987654321,
+        username = "CitizenKingKong"
+    },
+}
