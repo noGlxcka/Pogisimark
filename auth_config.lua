751b51830778a2a83be1fa9097fbc8e86e52dc67
GlxckADev <159496597+noGlxcka@users.noreply.github.com>
2024-07-27 17:58:19 +0800

    Update auth_config.lua

--- auth_config.lua
+++ auth_config.lua
@@ -7,4 +7,36 @@ return {
         user_id = 987654321,
         username = "CitizenKingKong"
     },
+    ["GAR-2LJD-5KHE"] = {
+        user_id = 1234567,
+        username = "ExampleUser1"
+    },
+    ["GAR-3XYZ-8ABC"] = {
+        user_id = 2345678,
+        username = "ExampleUser2"
+    },
+    ["GAR-4DEF-9GHI"] = {
+        user_id = 3456789,
+        username = "ExampleUser3"
+    },
+    ["GAR-5JKL-0MNO"] = {
+        user_id = 4567890,
+        username = "ExampleUser4"
+    },
+    ["GAR-6PQR-1STU"] = {
+        user_id = 5678901,
+        username = "ExampleUser5"
+    },
+    ["GAR-7VWX-2YZA"] = {
+        user_id = 6789012,
+        username = "ExampleUser6"
+    },
+    ["GAR-8BCD-3EFG"] = {
+        user_id = 7890123,
+        username = "ExampleUser7"
+    },
+    ["GAR-9HIJ-4KLM"] = {
+        user_id = 8901234,
+        username = "ExampleUser8"
+    },
 }
